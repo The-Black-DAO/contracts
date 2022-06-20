9fefdb255daad88c1a5b1adf4311f844b84bf9c0
wasif28 <wasif28@hotmail.com>
2022-06-20 16:27:24 +0500

    all contracts deployed and verified

--- scripts/setContracts.js
+++ scripts/setContracts.js
@@ -21,12 +21,12 @@ async function main() {
     const authorityAddress = "0x12275F627852aACa259B6752BdC8e60FC28EEca6";
     const blkdAddress = "0xe7DA1d8327AE1F320BbC23ca119b02AD3f85dceB";
     const sblkdAddress = "0x76B7B4062d7b518C79f324b6F1f9Cc4E10ABD27F";
-    const treasuryAddress = "";
-    const gblkdAddress = "";
-    const stakingAddress = "";
-    const distributorAddress = "";
-    const bondingCalculatorAddress = "";
-    const bondDepositoryAddress = "";
+    const treasuryAddress = "0xD088F31a21252308640CF9B6E70a13A7D79A56Ee";
+    const gblkdAddress = "0xfbf24289d776899482bf7aC33dEF116C4a551Fed";
+    const stakingAddress = "0x51b5cC83DD03e5aB10FdF06Bdd9EbBB7ef1B8c76";
+    const distributorAddress = "0xE1d162403f3157300fFE0E70aD59864F0F799fFD";
+    const bondingCalculatorAddress = "0x45981AF41f0AFFbB14c63888e3Cd10D4AA0C500B";
+    const bondDepositoryAddress = "0xcDc59231032737190aF627d2A7e643ED210426fC";
 
     const authority = await ethers.getContractAt("BlackDAOAuthority", authorityAddress);
     const blkd = await ethers.getContractAt("BlackDAOERC20Token", blkdAddress);
